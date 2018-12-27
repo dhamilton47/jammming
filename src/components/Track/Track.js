eb25e19b1c1bafa9b279a6b6ea429dc92fa17594
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2018-12-26 19:51:25 -0500

    Pass props into Track
    Props originate in TrackList.

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './Track.css';
 
-export class Track extends Component {
+class Track extends Component {
 	render() {
 		return (
 			<div className="Track">
 				<div className="Track-information">
-					<h3>Stronger</h3>
-					<p>Britney Spears | Oops!... I Did It Again</p>
+					<h3>{this.props.track.track}</h3>
+					<p>{this.props.track.artist} | {this.props.track.album}</p>
 				</div>
-				<a className="Track-action">-</a>
+				<a className="Track-action">{this.props.track.action}</a>
 			</div>
 		);
 	}
 }
+
+export default Track;
